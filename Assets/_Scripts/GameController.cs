94ac156e7a4cbef434d2b06e1869ad8e6b89bb6f
buntu88 <buntu88@gmail.com>
2016-02-03 20:32:02 -0500

    Button Added

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -7,11 +7,22 @@ public class GameController : MonoBehaviour {
     private int _scoreValue;
     private int _livesValue;
 
+
+    [SerializeField]
+    private AudioSource _gameOver;
+
     //PUBLIC INSTANCE VARIABLE
     public int enemyNumber = 3;
-    public EnemyController enemy;
     public Text Lives;
     public Text Points;
+    public Text GameOverLabel;
+    public Text HighScoreLabel;
+    public ShipController ship;
+    public PlanetController planet;
+    public EnemyController enemy;
+    public Button ResetButton;
+
+
     // PUBLIC ACCESS METHODS
     public int ScoreValue
     {
@@ -23,7 +34,9 @@ public class GameController : MonoBehaviour {
         set
         {
             this._scoreValue = value;
+            
             this.Points.text = "Score: "+ this._scoreValue;
+            
         }
     }
 
@@ -38,7 +51,13 @@ public class GameController : MonoBehaviour {
         set
         {
             this._livesValue = value;
-            this.Lives.text = "Lives: " + this._livesValue;
+            if (this._livesValue <= 0)
+            {
+                this._endGame();
+            }
+                this.Lives.text = "Lives: " + this._livesValue;
+            
+
         }
     }
 
@@ -46,14 +65,15 @@ public class GameController : MonoBehaviour {
     // Use this for initialization
     void Start () {
         this._initialize();
-	}
+    }
 
     //PRIVATE METHODS
     private void _initialize()
     {
         this.ScoreValue = 0;
         this.LivesValue = 5;
-
+        this.GameOverLabel.enabled = false;
+        this.ResetButton.gameObject.SetActive(false);
         for (int enemyCount = 0; enemyCount < this.enemyNumber; enemyCount++)
         {
             Instantiate(enemy.gameObject);
@@ -64,4 +84,23 @@ public class GameController : MonoBehaviour {
 	void Update () {
 	
 	}
+
+    private void _endGame()
+    {
+        this.HighScoreLabel.text = "High Scrore: " + this._scoreValue;
+        this.GameOverLabel.enabled = true;
+        this.HighScoreLabel.enabled = true;
+        this._gameOver.Play();
+        this.ship.gameObject.SetActive(false);
+        this.planet.gameObject.SetActive(false);
+        this.Points.enabled = false;
+        this.Lives.enabled = false;
+        this.ResetButton.gameObject.SetActive(true);
+
+    }
+
+    public void RestartButtonClick()
+    {
+        Application.LoadLevel("Main");
+    }
 }
